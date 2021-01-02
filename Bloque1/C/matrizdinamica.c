fc9a4f3ab66eac59208920bfc8a4f6edd9b45d2b
Mauricio_de_Garay <mauriciodg00@gmail.com>
2021-01-02 12:57:09 -0600

    Matriz dinamic

---
+++ Bloque1/C/matrizdinamica.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+void inicializarMatriz(int ***Matriz, int *filas, int *columnas);
+void llenarMatriz(int **Matriz, int filas, int columnas);
+void imprimirMatriz(int **Matriz, int filas, int columnas);
+int main()
+{
+    int **Matriz;
+    int filas, columnas;
+    int i;
+    inicializarMatriz(&Matriz, &filas, &columnas);
+    llenarMatriz(Matriz, filas, columnas);
+    imprimirMatriz(Matriz, filas, columnas);
+    for(i=0; i<filas; i++)
+    {
+        free(Matriz[i]);
+    }
+    free(Matriz);
+}
+void inicializarMatriz(int ***Matriz, int *filas, int *columnas)
+{
+    int i;
+    printf("Dame tus filas: ");
+    scanf("%d", filas);
+    fflush(stdin);
+    printf("Dame tus columnas: ");
+    scanf("%d", columnas);
+    fflush(stdin);
+    *Matriz=malloc((*filas)*sizeof(int*));
+    for(i=0; i<*columnas; i++)
+    {
+        *(*Matriz+i)=malloc((*columnas)*sizeof(int));
+    }
+}
+void llenarMatriz(int **Matriz, int filas, int columnas)
+{
+    int i, j, numero;
+    numero=1;
+    for(i=0; i<filas; i++)
+    {
+        for(j=0; j<columnas; j++)
+        {
+            Matriz[i][j]=numero;
+            numero++;
+        }
+    }
+}
+void imprimirMatriz(int **Matriz, int filas, int columnas)
+{
+    int i, j;
+    printf("Aqui esta tu matriz: \n");
+    for(i=0; i<filas; i++)
+    {
+        printf("\t");
+        for(j=0; j<columnas; j++)
+        {
+            printf("%03d ", Matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
