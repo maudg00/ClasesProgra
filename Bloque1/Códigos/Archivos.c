10e6f496a6f777affdff7e7590e5e4fccd990118
Francisco Rodríguez Arana <franin9@gmail.com>
2021-01-07 12:27:25 -0600

    Archivos.c

---
+++ Bloque1/Códigos/Archivos.c
@@ -0,0 +1,26 @@
+#include "utilities.h"
+void Escribir_Archivo(int *Arreglo, int tamanio);
+int main()
+{
+    int *Arreglo, tamanio;
+    Arreglo=NULL;
+    tamanio=utilitiesPedirEntero();
+    utilitiesDeclararArregloEntero(&Arreglo, tamanio);
+    utilitiesLlenarArregloEnteros(Arreglo, tamanio);
+    Escribir_Archivo(Arreglo, tamanio);
+}
+void Escribir_Archivo(int *Arreglo, int tamanio)
+{
+    FILE *Archivo1;
+    char nombrearch[30];
+    int i;
+    printf("Dime el nombre del archivo: ");
+    fgets(nombrearch, 28, stdin);
+    utilitiesQuitarEnterString(nombrearch);
+    Archivo1=fopen(nombrearch, "wt");
+    for (i = 0; i < tamanio; i++)
+    {
+        fprintf(Archivo1, "Numero %d: %d\n", i+1, Arreglo[i]);
+    }
+    fclose(Archivo1);
+}
