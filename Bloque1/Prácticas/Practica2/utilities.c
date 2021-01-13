f318b289a76aab042058a11a97faaa23354963a1
José Luis Aguilar Charfén <jlacharfen@gmail.com>
2021-01-13 10:55:05 -0600

    Agrego utilities a la carpeta.

---
+++ Bloque1/Prácticas/Practica2/utilities.c
@@ -0,0 +1,236 @@
+#include "utilities.h"
+/*
+* La función utilitiesPedirEntero  se encarga de pedir un numero entero al usuario
+*
+*
+* @params 
+* 
+*
+* @returns un int que se leyo del usuario.
+*
+*
+*/
+int utilitiesPedirEntero()
+{
+    int num;
+    printf("Dame Entero: ");
+    scanf("%d", &num);
+    fpurge(stdin);
+    return num;
+}
+/*
+* La función utilitiesPedirFlotante  se encarga de pedir un numero flotante al usuario
+*
+*
+* @param
+* 
+*
+* @returns un float que se leyo del usuario.
+*
+*
+*/
+float utilitiesPedirFlotante()
+{
+    float num;
+    printf("Dame flotante: ");
+    scanf("%f", &num);
+    fpurge(stdin);
+    return num;
+}
+/*
+* La función utilitiesValidarEntero  se encarga de  validar si un numero es entero (no tiene ningún valor después del punto decimal)
+*
+*
+* @param {float} [numero]: un numero flotante.
+* 
+*
+* @return {int}: 0 (FALSE) si no es entero, 
+* @return {int}: 1 (TRUE) si sí es entero.
+*
+*
+*/
+int utilitiesValidarEntero(float numero)
+{
+    int numeroEntero=(int)numero;
+    if(numeroEntero-numero!=0)
+    {
+        return FALSE;
+    }
+    else
+    {
+        return TRUE;
+    }
+    
+}
+/*
+* La función utilitiesValidarEnteroPositivo  se encarga de validar que un numero entero sea positivo mayor a 0.
+*
+*
+* @param int numero: numero entero a validar.
+* 
+*
+* @return int: 0 (FALSE) si no es positivo, 1 (TRUE) si sí lo es.
+*
+*
+*/
+int utilitiesValidarEnteroPositivo(int numero)
+{
+    if(numero<=0)
+    {
+        return FALSE;
+    }
+    return TRUE;
+}
+/*
+* La función utilitiesEnter  se encarga de esperar a que el usuario ingrese <enter> para continuar.
+*
+*
+* @param
+* 
+*
+* @return
+*
+*
+*/
+void utilitiesEnter()
+{
+    char enter;
+    printf("Presiona [Enter] para continuar... ");
+    scanf("%c", &enter);
+    fpurge(stdin);
+}
+/*
+* La función utilitiesQuitarEnterString se encarga de quitar el caracter '\n' a un string que lo tenga al final.
+*
+*
+* @param {char*} [string]: string al que le quitaremos el enter.
+* 
+* @return
+*
+*/
+void utilitiesQuitarEnterString(char * string)
+{
+    string[strlen(string)-1]=0;
+}
+/*
+* La función utilitiesStringToUpper se encarga de convertir todos los caracteres de un string a mayúsculas.
+*
+*
+* @param char* string: el string a convertir.
+* 
+*
+* @returns
+*
+*
+*/
+void utilitiesStringToUpper(char *string)
+{
+    int tamano=(int)strlen(string);
+    for(int i=0; i<tamano; i++)
+    {
+        string[i]=toupper(string[i]);
+    }
+}
+/*
+* La función utilitiesStringToLower se encarga de convertir todos los caracteres de un string a minúsculas.
+*
+*
+* @param char* string: el string a convertir.
+* 
+*
+* @returns
+*
+*
+*/
+void utilitiesStringToLower(char *string)
+{
+    int tamano=(int)strlen(string);
+    for(int i=0; i<tamano; i++)
+    {
+        string[i]=tolower(string[i]);
+    }
+}
+/*
+* La función utilitiesDeclararArregloEntero se encarga de declarar dinamicamente un arreglo entero.
+*
+*
+* @param int** arreglo, la direccion de memoria del arreglo a declarar.
+* @param int tamano, el tamano del arreglo.
+*
+* @returns
+*
+*/
+void utilitiesDeclararArregloEntero(int **arreglo, int tamano)
+{
+    *arreglo=malloc((tamano)*sizeof(int));
+}
+void utilitiesLlenarArregloEnteros(int * arreglo, int tamano)
+{
+    for(int i=0; i<tamano; i++)
+    {
+        printf("Dame el valor %d del arreglo: ", (i+1));
+        scanf("%d", arreglo+i);
+        fpurge(stdin);
+    }
+}
+/*
+* La función utilitiesImprimirArregloEnteros se encarga de imprimir un arreglo de enteros
+*
+*
+* @param int* arreglo, el arreglo de enteros.
+* @param int tamano, el tamano del arreglo
+* 
+*
+* @returns
+*
+*
+*/
+void utilitiesImprimirArregloEnteros(int * arreglo, int tamano)
+{
+    for(int i=0; i<tamano; i++)
+    {
+        printf("Arreglo[%d]=%d", (i+1), *(arreglo+i));
+    }
+}
+/*
+* La función utilitiesDeclararMatrizEnteros se encarga de declarar una matriz dinámica.
+*
+*
+* @param int*** matriz, la dirección de memoria de la matriz.
+* @param int filas, las filas de la matriz.
+* @param int columnas, las columnas de la matriz.
+* 
+* @returns
+*
+*/
+void utilitiesDeclararMatrizEnteros(int ***Matriz, int filas, int columnas)
+{
+    int i;
+    *Matriz=malloc((filas)*sizeof(int*));
+    for(i=0; i<columnas; i++)
+    {
+        *(*Matriz+i)=malloc((columnas)*sizeof(int));
+    }
+}
+/*
+* La función utilitiesLiberarMatrizEnteros se encarga de liberar una matriz de enteros que fue inicializada dinámicamente.
+*
+*
+* @params
+*   @param int** Matriz, la dirección de memoria de la matriz.
+*   @param int filas, las filas de la matriz.
+*   @param int columnas, las columnas de la matriz.
+* 
+*
+* @returns
+*
+*
+*/
+void utilitiesLiberarMatrizEnteros(int **Matriz, int filas, int columnas)
+{
+    for(int i=0; i<filas; i++)
+    {
+        free(Matriz[i]);
+    }
+    free(Matriz);
+}
---
+++ Bloque1/Prácticas/Practica2/utilities.h
@@ -0,0 +1,190 @@
+// utilities.h
+//
+// 
+//  Created by Mauricio de Garay, Tania Cesin, Kirya Hernandez, Jose Luis on 06/01/2021.
+// @author Mauricio de Garay <mauriciodg2000@gmail.com>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define TRUE 1
+#define FALSE 0
+#ifndef utilities_h
+#define utilities_h
+#ifndef utilities_IMPORT
+    #define EXTERN
+#else
+    #define EXTERN extern
+#endif
+/* Function prototypes. */
+
+
+/*
+* La función utilitiesPedirEntero  se encarga de pedir un numero entero al usuario
+*
+*
+* @param
+* 
+*
+* @returns int que se leyo del usuario.
+*
+*
+*/
+EXTERN int utilitiesPedirEntero();
+/*
+* La función utilitiesPedirFlotante  se encarga de pedir un numero flotante al usuario
+*
+*
+* @param
+* 
+*
+* @returns un float que se leyo del usuario.
+*
+*
+*/
+EXTERN float utilitiesPedirFlotante();
+/*
+* La función utilitiesValidarEntero  se encarga de  validar si un numero es 
+* entero (no tiene ningún valor después del punto decimal)
+*
+*
+* @param float numero: un numero flotante.
+* 
+*
+* @return int 0 (FALSE) si no es entero, 1 (TRUE) si sí es entero.
+*
+*
+*/
+EXTERN int utilitiesValidarEntero(float numero);
+/*
+* La función utilitiesValidarEnteroPositivo  se encarga de validar que un numero entero sea positivo mayor a 0.
+*
+*
+* @param int numero: numero entero a validar.
+* 
+*
+* @return int: 0 (FALSE) si no es positivo, 1 (TRUE) si sí lo es.
+*
+*
+*/
+EXTERN int utilitiesValidarEnteroPositivo(int numero);
+/*
+* La función utilitiesEnter  se encarga de esperar a que el usuario ingrese <enter> para continuar.
+*
+*
+* @param
+* 
+*
+* @returns
+*
+*
+*/
+EXTERN void utilitiesEnter();
+/*
+* La función utilitiesQuitarEnterString se encarga de quitar el caracter '\n' a un string que lo tenga al final.
+*
+*
+* @param char* string: string al que le quitaremos el enter.
+* 
+*
+* @return
+*
+*
+*/
+EXTERN void utilitiesQuitarEnterString(char * string);
+/*
+* La función utilitiesStringToUpper se encarga de convertir todos los caracteres de un string a mayúsculas.
+*
+*
+* @param char* string: el string a convertir.
+* 
+*
+* @returns
+*
+*
+*/
+EXTERN void utilitiesStringToUpper(char *string);
+/*
+* La función utilitiesStringToLower se encarga de convertir todos los caracteres de un string a minúsculas.
+*
+*
+* @param char* string: el string a convertir.
+* 
+*
+* @returns
+*
+*
+*/
+EXTERN void utilitiesStringToLower(char *string);
+/*
+* La función utilitiesDeclararArregloEntero se encarga de declarar dinamicamente un arreglo entero.
+*
+*
+* @param int** arreglo, la direccion de memoria del arreglo a declarar.
+* @param int tamano, el tamano del arreglo.
+* 
+* @return
+*
+*
+*/
+EXTERN void utilitiesDeclararArregloEntero(int **arreglo, int tamano);
+/*
+* La función utilitiesLlenarArregloEnteros se encarga de llenar un arreglo.
+*
+*
+* @param int * arreglo, el arreglo de enteros.
+* @param int tamano, el tamano del arreglo
+* 
+*
+* @return
+*
+*
+*/
+EXTERN void utilitiesLlenarArregloEnteros(int * arreglo, int tamano);
+/*
+* La función utilitiesImprimirArregloEnteros se encarga de imprimir un arreglo de enteros
+*
+*
+* @params
+*   @param int* arreglo, el arreglo de enteros.
+*   @param int tamano, el tamano del arreglo
+* 
+*
+* @returns
+*
+*
+*/
+EXTERN void utilitiesImprimirArregloEnteros(int * arreglo, int tamano);
+/*
+* La función utilitiesDeclararMatrizEnteros se encarga de declarar una matriz dinámica.
+*
+*
+* @params
+*   @param int*** Matriz, la dirección de memoria de la matriz.
+*   @param int filas, las filas de la matriz.
+*   @param int columnas, las columnas de la matriz.
+* 
+*
+* @returns
+*
+*
+*/
+EXTERN void utilitiesDeclararMatrizEnteros(int ***Matriz, int filas, int columnas);
+/*
+* La función utilitiesLiberarMatrizEnteros se encarga de liberar una matriz de enteros que fue inicializada dinámicamente.
+*
+*
+* @params
+*   @param int** Matriz, la matriz a liberar.
+*   @param int filas, las filas de la matriz.
+*   @param int columnas, las columnas de la matriz.
+* 
+*
+* @returns
+*
+*
+*/
+EXTERN void utilitiesLiberarMatrizEnteros(int **Matriz, int filas, int columnas);
+#undef utilities_IMPORT
+#undef EXTERN
+#endif
